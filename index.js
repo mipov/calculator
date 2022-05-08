6ba8493975ba25d41fd3a1bb95e1f749a2f2a4e9
Mikhail Popov <mipo.spb@gmail.com>
2022-05-09 00:44:14 +0300

    make symple calculator

--- index.js
+++ index.js
@@ -1,39 +1,48 @@
 const displayInput = document.querySelector('.display__input');
 const displayOperator = document.querySelector('.display__operator');
 const displaySum = document.querySelector('.display__sum');
+
 const button = document.querySelectorAll('.button')
+const btnEqual = document.querySelector('.equal-button');
 
-const btnOperator = document.querySelectorAll('.button_operator');
-const btnNumber = document.querySelectorAll('.button_number');
-const btnEqual = document.querySelector('.equal_button');
-
-
-function make() {
-  let str = '';
-  button.forEach(
-    function(num) {
-      num.addEventListener('click', () => {
-        str = str + num.textContent;
-        console.log(str)
-        displayInput.textContent = str;
-        if (num.textContent == 'C') {
-          displayInput.textContent = '0';
-          displaySum.textContent = '';
-          displayOperator.textContent = '';
-          str = '';
-        } else if (
-          num.textContent == '+' ||
-          num.textContent == '−' ||
-          num.textContent == '✕' ||
-          num.textContent == '÷') {
-          displaySum.textContent = str.slice(0, -1);
-          displayOperator.textContent = str.slice(-1)
-          displayInput.textContent = '';
-          str = '';
-        }
-      })
-    }
-  )
-}
-
-make();
+button.forEach(
+  function (num) {
+    num.addEventListener('click', () => {
+      displayInput.textContent == '0' ?
+        displayInput.textContent = num.textContent :
+        displayInput.textContent = displayInput.textContent + num.textContent;
+      if (num.textContent == 'C') {
+        displayInput.textContent = '0';
+        displaySum.textContent = '';
+        displayOperator.textContent = '';
+      } else if (
+        num.textContent == '+' ||
+        num.textContent == '−' ||
+        num.textContent == '✕' ||
+        num.textContent == '÷') {
+        displaySum.textContent = displayInput.textContent.slice(0, -1);
+        displayOperator.textContent = num.textContent.slice(-1)
+        displayInput.textContent = '';
+      }
+    })
+  }
+)
+btnEqual.addEventListener('click', () => {
+  switch (displayOperator.textContent) {
+    case '+':
+      sum = +displaySum.textContent + +displayInput.textContent;
+      break;
+    case '−':
+      sum = +displaySum.textContent - +displayInput.textContent;
+      break;
+    case '✕':
+      sum = +displaySum.textContent * +displayInput.textContent;
+      break;
+    case '÷':
+      sum = +displaySum.textContent / +displayInput.textContent;
+      break;
+  }
+  displayInput.textContent = Math.floor(sum * 100) / 100;
+  displaySum.textContent = Math.floor(sum * 100) / 100;
+  displayOperator.textContent = ''
+})
